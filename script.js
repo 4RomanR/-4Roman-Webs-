48e70f9084d457401567c0d31497b48bfa11c7c8
Roman <4rprogramming@gmail.com>
2022-08-19 12:34:59 -0500

    javascript

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 /*VAR LET CONST*/
-const burgerMenu = document.querySelector('.burger-nav-content');
-const imgBurger = document.querySelector('.burger-content-img');
 
-imgBurger.addEventListener('click',openBurgerMenu);
-
-/*---------FUNCTIONS---------*/
 
 //BURGER
-function {}(
-    openBurgerMenu.classList('toggle');
-)
+const burgerMenu = document.querySelector('.burger-nav-content');
+const imgBurger  = document.querySelector('.burger-img');
+
+imgBurger.addEventListener('click', openBurgerMenu);
+
+function openBurgerMenu (){
+    burgerMenu.classList.toggle('inactive');
+}
+
+//CTA
+const cta1 = document.querySelector('.cta-change-1');
+const cta2 = document.querySelector('.cta-change-2');
+const cta3 = document.querySelector('.cta-change-3');
+
+const ctaGreen = document.querySelector('.cta-green-content');
+const ctaRed   = document.querySelector('.cta-red-content');
+const ctaBlue  = document.querySelector('.cta-blue-content');
+
+cta1.addEventListener('mouseover', openCta);
+
+    function openCta (){
+        ctaGreen.classList.remove('inactive-cta');
+        ctaRed.classList.add('inactive-cta');
+        ctaBlue.classList.add('inactive-cta');
+    };
+
+cta2.addEventListener('mouseover', openCta);
+
+    function openCta (){
+        ctaRed.classList.remove('inactive-cta');
+        ctaGreen.classList.add('inactive-cta');
+        ctaBlue.classList.add('inactive-cta');
+    };
+
+
+cta3.addEventListener('mouseover', openCta);
+
+    function openCta (){
+        ctaBlue.classList.remove('inactive-cta');
+        ctaRed.classList.add('inactive-cta');
+        ctaGreen.classList.add('inactive-cta');
+    };
+    
