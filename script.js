b40ea7305788c9a030dafd3064df93371f6f3a52
Roman <4rprogramming@gmail.com>
2022-08-19 08:52:09 -0500

    javascript a

---
+++ script.js
@@ -0,0 +1,12 @@
+/*VAR LET CONST*/
+const burgerMenu = document.querySelector('.burger-nav-content');
+const imgBurger = document.querySelector('.burger-content-img');
+
+imgBurger.addEventListener('click',openBurgerMenu);
+
+/*---------FUNCTIONS---------*/
+
+//BURGER
+function {}(
+    openBurgerMenu.classList('toggle');
+)
