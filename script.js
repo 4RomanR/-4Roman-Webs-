2b1c84012f1b15ef22255bf5e25ae249529e8131
Roman <4rprogramming@gmail.com>
2022-08-19 19:15:31 -0500

    css javascript

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-/*VAR LET CONST*/
-
+(function (){
 
 //BURGER
-const burgerMenu = document.querySelector('.burger-nav-content');
-const imgBurger  = document.querySelector('.burger-img');
+var burgerMenu = document.querySelector('.burger-nav-content');
+var imgBurger  = document.querySelector('.burger-img');
 
 imgBurger.addEventListener('click', openBurgerMenu);
 
@@ -11,28 +10,35 @@ function openBurgerMenu (){
     burgerMenu.classList.toggle('inactive');
 }
 
+}())
+
+
 //CTA
+
+const ctaChange = document.querySelector('.main-cta-container');
+
+
 const cta1 = document.querySelector('.cta-change-1');
 const cta2 = document.querySelector('.cta-change-2');
 const cta3 = document.querySelector('.cta-change-3');
 
-const ctaGreen = document.querySelector('.cta-green-content');
-const ctaRed   = document.querySelector('.cta-red-content');
+const ctaGreen = document.querySelector('.img-content-green');
+const ctaRed   = document.querySelector('.img-content-red');
 const ctaBlue  = document.querySelector('.cta-blue-content');
 
 cta1.addEventListener('mouseover', openCta);
 
     function openCta (){
-        ctaGreen.classList.remove('inactive-cta');
-        ctaRed.classList.add('inactive-cta');
+        ctaGreen.classList.remove('inactcta');
+        ctaRed.classList.add('inactcta');
         ctaBlue.classList.add('inactive-cta');
     };
 
 cta2.addEventListener('mouseover', openCta);
 
     function openCta (){
-        ctaRed.classList.remove('inactive-cta');
-        ctaGreen.classList.add('inactive-cta');
+        ctaRed.classList.remove('inactcta');
+        ctaGreen.classList.add('inactcta');
         ctaBlue.classList.add('inactive-cta');
     };
 
@@ -44,4 +50,3 @@ cta3.addEventListener('mouseover', openCta);
         ctaRed.classList.add('inactive-cta');
         ctaGreen.classList.add('inactive-cta');
     };
-    
